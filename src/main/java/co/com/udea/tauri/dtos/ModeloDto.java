24b9509ca0b75352cf54d6c9b09d358fc5fd1665
Daniel Serna <daniel.serna@sancorsalud.com.ar>
2022-05-05 08:57:47 -0500

    ajuste a la biblioteca y avance modelo

--- src/main/java/co/com/udea/tauri/dtos/ModeloDto.java
+++ src/main/java/co/com/udea/tauri/dtos/ModeloDto.java
@@ -3,6 +3,10 @@ package co.com.udea.tauri.dtos;
 public class ModeloDto {
 	
 	private Double actualDMI;
+	private Double totalDMFeed;
+	private Double scurfRequirement;
+	private Double urinaryRequirement;
+	private Double metabolicFecalProteinReq;
 
 	public Double getActualDMI() {
 		return actualDMI;
@@ -10,6 +14,38 @@ public class ModeloDto {
 
 	public void setActualDMI(Double actualDMI) {
 		this.actualDMI = actualDMI;
+	}
+
+	public Double getTotalDMFeed() {
+		return totalDMFeed;
+	}
+
+	public void setTotalDMFeed(Double totalDMFeed) {
+		this.totalDMFeed = totalDMFeed;
+	}
+
+	public Double getScurfRequirement() {
+		return scurfRequirement;
+	}
+
+	public void setScurfRequirement(Double scurfRequirement) {
+		this.scurfRequirement = scurfRequirement;
+	}
+
+	public Double getUrinaryRequirement() {
+		return urinaryRequirement;
+	}
+
+	public void setUrinaryRequirement(Double urinaryRequirement) {
+		this.urinaryRequirement = urinaryRequirement;
+	}
+
+	public Double getMetabolicFecalProteinReq() {
+		return metabolicFecalProteinReq;
+	}
+
+	public void setMetabolicFecalProteinReq(Double metabolicFecalProteinReq) {
+		this.metabolicFecalProteinReq = metabolicFecalProteinReq;
 	}
 	
 }
--- src/main/java/co/com/udea/tauri/services/impl/ModeloService.java
+++ src/main/java/co/com/udea/tauri/services/impl/ModeloService.java
@@ -23,8 +23,13 @@ public class ModeloService implements IModeloService {
 	private static final Integer CANTIDAD_DECIMALES = 2, FCM_CONSTANT_INTEGER = 15, CONSTANT_CIEN = 100;
 	private static final Double KP_OF_WET_FORAGE_CONSTANT = 3.054, KP_OF_WET_FORAGE_CONSTANT_DOS = 0.614;
 	private static final Double KP_OF_WET_CONCENTRATE_CONSTANT = 2.904, KP_OF_WET_CONCENTRATE_CONSTANT_DOS = 1.375;
-	private static final Double KP_OF_WET_CONCENTRATE_CONSTANT_TRES = 0.02;
-	
+	private static final Double KP_OF_WET_CONCENTRATE_CONSTANT_TRES = 0.02, CS3EBW_CONSTANT = 0.96,
+			CS3EBW_CONSTANT_DOS = 0.851;
+	private static final Double CONSTANT_TOTAL_DM_FEED = 0.372, CONSTANT_TOTAL_DM_FEED_DOS = 0.0968,
+			CONSTANT_TOTAL_DM_FEED_TRES = 0.75, CONSTANT_TOTAL_DM_FEED_CUATRO = -0.192,
+			CONSTANT_TOTAL_DM_FEED_CINCO = 3.67;
+	private static final Double CONSTANT_SCURF_REQUIREMENT = 0.3, CONSTANT_SCURF_REQUIREMENT_DOS = 0.6;
+
 	@Autowired
 	private BibliotecaRepository bibliotecaRepository;
 
@@ -80,10 +85,58 @@ public class ModeloService implements IModeloService {
 						+ KP_OF_WET_FORAGE_CONSTANT_DOS * (cmsActual / entradaDto.getPesoCorporal() * CONSTANT_CIEN),
 				CANTIDAD_DECIMALES);
 		System.out.println(kpOfWetForage);
-		Double kpOfConcentrate = formatearDecimales(KP_OF_WET_CONCENTRATE_CONSTANT
-				+ KP_OF_WET_CONCENTRATE_CONSTANT_DOS * (cmsActual / entradaDto.getPesoCorporal() * CONSTANT_CIEN)
-				- KP_OF_WET_CONCENTRATE_CONSTANT_TRES*(cmsConcentrate/cmsActual*CONSTANT_CIEN), CANTIDAD_DECIMALES);
+		Double kpOfConcentrate = formatearDecimales(
+				KP_OF_WET_CONCENTRATE_CONSTANT
+						+ KP_OF_WET_CONCENTRATE_CONSTANT_DOS
+								* (cmsActual / entradaDto.getPesoCorporal() * CONSTANT_CIEN)
+						- KP_OF_WET_CONCENTRATE_CONSTANT_TRES * (cmsConcentrate / cmsActual * CONSTANT_CIEN),
+				CANTIDAD_DECIMALES);
 		System.out.println(kpOfConcentrate);
+		Double cs3Ebw = 0.0;
+		if ("Jersey".equals(entradaDto.getRaza())) {
+			cs3Ebw = formatearDecimales(MW_JERSEY * CS3EBW_CONSTANT * CS3EBW_CONSTANT_DOS, CANTIDAD_DECIMALES);
+		} else {
+			cs3Ebw = formatearDecimales(MW_HOLSTEIN * CS3EBW_CONSTANT * CS3EBW_CONSTANT_DOS, CANTIDAD_DECIMALES);
+		}
+		System.out.println(cs3Ebw);
+		Double bodyEnergy = 0.0;
+		if (new Double(1).equals(entradaDto.getCondicionCorporal())) {
+
+		}
+		System.out.println(bodyEnergy);
+		Double cw = formatearDecimales((18 + ((entradaDto.getDiasPrenez() - 190) * 0.665)) * (cbw / 45),
+				CANTIDAD_DECIMALES);
+		System.out.println(cw);
+
+		// calculos modelo
+		Double totalDmFeed = formatearDecimales(
+				(CONSTANT_TOTAL_DM_FEED * fcm + CONSTANT_TOTAL_DM_FEED_DOS
+						* Math.pow(entradaDto.getPesoCorporal(), CONSTANT_TOTAL_DM_FEED_TRES)
+						* (1 - Math.exp(CONSTANT_TOTAL_DM_FEED_CUATRO * (wol + CONSTANT_TOTAL_DM_FEED_CINCO)))),
+				CANTIDAD_DECIMALES);
+		Double scurfRequirement = 0.0;
+		Double urinaryRequirement = 0.0;
+		if (entradaDto.getDiasPrenez() < 190) {
+			scurfRequirement = formatearDecimales(
+					CONSTANT_SCURF_REQUIREMENT
+							* (Math.pow(entradaDto.getPesoCorporal(), CONSTANT_SCURF_REQUIREMENT_DOS)),
+					CANTIDAD_DECIMALES);
+			urinaryRequirement = formatearDecimales(4.1 * (Math.pow(entradaDto.getPesoCorporal(), 0.5)),
+					CANTIDAD_DECIMALES);
+		} else {
+			scurfRequirement = formatearDecimales(
+					CONSTANT_SCURF_REQUIREMENT
+							* (Math.pow(entradaDto.getPesoCorporal() - cw, CONSTANT_SCURF_REQUIREMENT_DOS)),
+					CANTIDAD_DECIMALES);
+			urinaryRequirement = formatearDecimales(4.1 * (Math.pow(entradaDto.getPesoCorporal() - cw, 0.5)), CANTIDAD_DECIMALES);
+		}
+		
+		Double metabolicFecalProteinReq = (cmsActual*1000*0.03-(0.5*((1260.12/0.8)-1260.12)));
+		System.out.println(metabolicFecalProteinReq);
+//		modeloDto.setActualDMI(cmsActual);
+		modeloDto.setTotalDMFeed(totalDmFeed);
+		modeloDto.setScurfRequirement(scurfRequirement);
+		modeloDto.setUrinaryRequirement(urinaryRequirement);
 		return modeloDto;
 	}
 
