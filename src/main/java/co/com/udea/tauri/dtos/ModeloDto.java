9e335b1d26fe9f11d4ac68dacb39d0513487eebf
Daniel Serna <daniel.serna@sancorsalud.com.ar>
2022-05-09 08:49:48 -0500

    fase 1 del modelo

--- src/main/java/co/com/udea/tauri/dtos/ModeloDto.java
+++ src/main/java/co/com/udea/tauri/dtos/ModeloDto.java
@@ -7,6 +7,12 @@ public class ModeloDto {
 	private Double scurfRequirement;
 	private Double urinaryRequirement;
 	private Double metabolicFecalProteinReq;
+	private Double mpEndougenosRequirement;
+	private Double mpMaint;
+	private Double mpLact;
+	private Double mpGrowth;
+	private Double mpPreg;
+	private Double totalMpRequirement;
 
 	public Double getActualDMI() {
 		return actualDMI;
@@ -46,6 +52,54 @@ public class ModeloDto {
 
 	public void setMetabolicFecalProteinReq(Double metabolicFecalProteinReq) {
 		this.metabolicFecalProteinReq = metabolicFecalProteinReq;
+	}
+
+	public Double getMpEndougenosRequirement() {
+		return mpEndougenosRequirement;
+	}
+
+	public void setMpEndougenosRequirement(Double mpEndougenosRequirement) {
+		this.mpEndougenosRequirement = mpEndougenosRequirement;
+	}
+
+	public Double getMpMaint() {
+		return mpMaint;
+	}
+
+	public void setMpMaint(Double mpMaint) {
+		this.mpMaint = mpMaint;
+	}
+
+	public Double getMpLact() {
+		return mpLact;
+	}
+
+	public void setMpLact(Double mpLact) {
+		this.mpLact = mpLact;
+	}
+
+	public Double getMpGrowth() {
+		return mpGrowth;
+	}
+
+	public void setMpGrowth(Double mpGrowth) {
+		this.mpGrowth = mpGrowth;
+	}
+
+	public Double getTotalMpRequirement() {
+		return totalMpRequirement;
+	}
+
+	public void setTotalMpRequirement(Double totalMpRequirement) {
+		this.totalMpRequirement = totalMpRequirement;
+	}
+
+	public Double getMpPreg() {
+		return mpPreg;
+	}
+
+	public void setMpPreg(Double mpPreg) {
+		this.mpPreg = mpPreg;
 	}
 	
 }
--- src/main/java/co/com/udea/tauri/services/impl/ModeloService.java
+++ src/main/java/co/com/udea/tauri/services/impl/ModeloService.java
@@ -71,15 +71,24 @@ public class ModeloService implements IModeloService {
 		System.out.println(cbw);
 		Double cmsActual = 0.0;
 		Double cmsConcentrate = 0.0;
+		Double tdn = 0.0;
+		Double sumaTdn = 0.0;
+		Double cpIntake = 0.0;
 		for (DietaDto dietaDto : dietaDtos) {
 			cmsActual = cmsActual + dietaDto.getCantidad();
 			Biblioteca biblioteca = bibliotecaRepository.findById(dietaDto.getIdBiblioteca()).get();
 			if (biblioteca != null) {
+				tdn = formatearDecimales((dietaDto.getCantidad() * (biblioteca.getEd() / 0.04409)) / CONSTANT_CIEN,
+						CANTIDAD_DECIMALES);
+				sumaTdn = sumaTdn + tdn;
 				if ("Concentrado".equals(biblioteca.getTipo())) {
 					cmsConcentrate = cmsConcentrate + dietaDto.getCantidad();
 				}
 			}
 		}
+		Double tdnOriginal = formatearDecimales(((sumaTdn / cmsActual) * CONSTANT_CIEN), CANTIDAD_DECIMALES);
+		Double nel = formatearDecimales((tdnOriginal * 0.0245) - 0.12, CANTIDAD_DECIMALES);
+
 		Double kpOfWetForage = formatearDecimales(
 				KP_OF_WET_FORAGE_CONSTANT
 						+ KP_OF_WET_FORAGE_CONSTANT_DOS * (cmsActual / entradaDto.getPesoCorporal() * CONSTANT_CIEN),
@@ -108,7 +117,6 @@ public class ModeloService implements IModeloService {
 				CANTIDAD_DECIMALES);
 		System.out.println(cw);
 
-		// calculos modelo
 		Double totalDmFeed = formatearDecimales(
 				(CONSTANT_TOTAL_DM_FEED * fcm + CONSTANT_TOTAL_DM_FEED_DOS
 						* Math.pow(entradaDto.getPesoCorporal(), CONSTANT_TOTAL_DM_FEED_TRES)
@@ -116,6 +124,34 @@ public class ModeloService implements IModeloService {
 				CANTIDAD_DECIMALES);
 		Double scurfRequirement = 0.0;
 		Double urinaryRequirement = 0.0;
+		Double neMaint = 0.0;
+		Double porcentajeRdp = 0.0;
+		Double rdp = 0.0, sumaRdp = 0.0;
+		for (DietaDto dietaDto : dietaDtos) {
+			Biblioteca biblioteca = bibliotecaRepository.findById(dietaDto.getIdBiblioteca()).get();
+			if (biblioteca != null) {
+				cpIntake = formatearDecimales(dietaDto.getCantidad() * biblioteca.getFdn() / 100 * 1000,
+						CANTIDAD_DECIMALES);
+				if (new Double(0).equals(dietaDto.getCantidad())) {
+					porcentajeRdp = 0.0;
+				}
+				if ("Forraje".equals(biblioteca.getTipo())) {
+					porcentajeRdp = formatearDecimales(
+							biblioteca.getFraccionA() + (biblioteca.getFraccionB()
+									* (biblioteca.getKdFraccionB() / (biblioteca.getKdFraccionB() + kpOfWetForage))),
+							CANTIDAD_DECIMALES);
+				} else {
+					if ("Concentrado".equals(biblioteca.getTipo())) {
+						porcentajeRdp = formatearDecimales(biblioteca.getFraccionA() + (biblioteca.getFraccionB()
+								* (biblioteca.getKdFraccionB() / (biblioteca.getKdFraccionB() + kpOfConcentrate))),
+								CANTIDAD_DECIMALES);
+					}
+				}
+			}
+			rdp = cpIntake * porcentajeRdp / 100;
+			sumaRdp = sumaRdp + rdp;
+		}
+
 		if (entradaDto.getDiasPrenez() < 190) {
 			scurfRequirement = formatearDecimales(
 					CONSTANT_SCURF_REQUIREMENT
@@ -123,20 +159,121 @@ public class ModeloService implements IModeloService {
 					CANTIDAD_DECIMALES);
 			urinaryRequirement = formatearDecimales(4.1 * (Math.pow(entradaDto.getPesoCorporal(), 0.5)),
 					CANTIDAD_DECIMALES);
+			if (new Double(0).equals(entradaDto.getDistancia()) || entradaDto.getDistancia() == null) {
+				neMaint = formatearDecimales(((Math.pow(entradaDto.getPesoCorporal(), 0.75) * 0.08)),
+						CANTIDAD_DECIMALES);
+			} else {
+				if (entradaDto.getTipografia() != null && entradaDto.getTipografia().equals("Plana")) {
+					neMaint = formatearDecimales(((Math.pow(entradaDto.getPesoCorporal(), 0.75) * 0.08))
+							+ ((((entradaDto.getDistancia() / 1000) * entradaDto.getNumeroViajes())
+									* (0.00045 * entradaDto.getPesoCorporal()))
+									+ (0.0012 * entradaDto.getPesoCorporal())),
+							CANTIDAD_DECIMALES);
+
+				} else {
+					neMaint = formatearDecimales(
+							((Math.pow(entradaDto.getPesoCorporal(), 0.75) * 0.08))
+									+ ((((entradaDto.getDistancia() / 1000) * entradaDto.getNumeroViajes())
+											* (0.00045 * entradaDto.getPesoCorporal()))
+											+ (0.0012 * entradaDto.getPesoCorporal()))
+									+ (0.006 * entradaDto.getPesoCorporal()),
+							CANTIDAD_DECIMALES);
+				}
+			}
 		} else {
 			scurfRequirement = formatearDecimales(
 					CONSTANT_SCURF_REQUIREMENT
 							* (Math.pow(entradaDto.getPesoCorporal() - cw, CONSTANT_SCURF_REQUIREMENT_DOS)),
 					CANTIDAD_DECIMALES);
-			urinaryRequirement = formatearDecimales(4.1 * (Math.pow(entradaDto.getPesoCorporal() - cw, 0.5)), CANTIDAD_DECIMALES);
+			urinaryRequirement = formatearDecimales(4.1 * (Math.pow(entradaDto.getPesoCorporal() - cw, 0.5)),
+					CANTIDAD_DECIMALES);
+			if (new Double(0).equals(entradaDto.getDistancia()) || entradaDto.getDistancia() == null) {
+				neMaint = formatearDecimales(((Math.pow(entradaDto.getPesoCorporal() - cw, 0.75) * 0.08)),
+						CANTIDAD_DECIMALES);
+			} else {
+				if (entradaDto.getTipografia() != null && entradaDto.getTipografia().equals("Plana")) {
+					neMaint = formatearDecimales(((Math.pow(entradaDto.getPesoCorporal() - cw, 0.75) * 0.08))
+							+ ((((entradaDto.getDistancia() / 1000) * entradaDto.getNumeroViajes())
+									* (0.00045 * entradaDto.getPesoCorporal()))
+									+ (0.0012 * entradaDto.getPesoCorporal())),
+							CANTIDAD_DECIMALES);
+				} else {
+					neMaint = formatearDecimales(
+							((Math.pow(entradaDto.getPesoCorporal() - cw, 0.75) * 0.08))
+									+ ((((entradaDto.getDistancia() / 1000) * entradaDto.getNumeroViajes())
+											* (0.00045 * entradaDto.getPesoCorporal()))
+											+ (0.0012 * entradaDto.getPesoCorporal()))
+									+ (0.006 * entradaDto.getPesoCorporal()),
+							CANTIDAD_DECIMALES);
+				}
+			}
+		}
+
+		Double dmiMaintenanceLevel = neMaint / nel;
+		Double intakeAboveMaintenance = cmsActual / dmiMaintenanceLevel;
+		Double tdnActX = formatearDecimales((tdnOriginal - ((0.18 * tdnOriginal) - 10.3) * intakeAboveMaintenance - 1),
+				CANTIDAD_DECIMALES);
+		Double nelAdjusted = formatearDecimales((tdnActX * 0.0245) - 0.12, CANTIDAD_DECIMALES);
+
+		Double mcpFromTdnDiscounted = formatearDecimales((tdnActX / 100) * cmsActual * 130, CANTIDAD_DECIMALES);
+		Double mcpFromTdnDiscountedRdp = sumaRdp * 0.85;
+		Double mcpAdjust = 0.0;
+		if (mcpFromTdnDiscounted > mcpFromTdnDiscountedRdp) {
+			mcpAdjust = formatearDecimales(mcpFromTdnDiscountedRdp, CANTIDAD_DECIMALES);
+		} else {
+			mcpAdjust = mcpFromTdnDiscounted;
+		}
+
+		Double mpBact = mcpAdjust * 0.64;
+
+		Double metabolicFecalProteinReq = formatearDecimales(
+				(cmsActual * 1000 * 0.03 - (0.5 * ((mpBact / 0.8) - mpBact))), CANTIDAD_DECIMALES);
+		Double mpEndoungeosRequirement = formatearDecimales((11.8*cmsActual*0.4)/0.67, CANTIDAD_DECIMALES);
+		
+		Double mpMaint = formatearDecimales(scurfRequirement + urinaryRequirement + metabolicFecalProteinReq + mpEndoungeosRequirement, CANTIDAD_DECIMALES);
+		
+		Double mpLact = formatearDecimales((yprotn/0.67)*1000, CANTIDAD_DECIMALES);
+		
+		Double mpPreg = formatearDecimales((((0.69*entradaDto.getDiasPrenez())-69.2)*(cbw/45))/0.33, CANTIDAD_DECIMALES);
+		
+		Double meanTargetSbw = formatearDecimales(entradaDto.getPesoCorporal()*0.96, CANTIDAD_DECIMALES);
+		Double eqsbw = 0.0;
+		if ("Jersey".equals(entradaDto.getRaza())) {
+			eqsbw = formatearDecimales(meanTargetSbw*(478/(MW_JERSEY*0.96)), CANTIDAD_DECIMALES);
+		} else {
+			eqsbw = formatearDecimales(meanTargetSbw*(478/(MW_HOLSTEIN*0.96)), CANTIDAD_DECIMALES);
 		}
 		
-		Double metabolicFecalProteinReq = (cmsActual*1000*0.03-(0.5*((1260.12/0.8)-1260.12)));
-		System.out.println(metabolicFecalProteinReq);
-//		modeloDto.setActualDMI(cmsActual);
+		Double eqebw = formatearDecimales(eqsbw*0.891, CANTIDAD_DECIMALES);
+		Double eqebeExp = formatearDecimales(Math.pow(eqebw, 0.75), CANTIDAD_DECIMALES);
+		Double swg = formatearDecimales( entradaDto.getGananciaPeso()*0.96, CANTIDAD_DECIMALES);
+		Double eqebg = formatearDecimales(swg*0.956, CANTIDAD_DECIMALES);
+		Double eqebgExp = formatearDecimales(Math.pow(eqebg, 1.097), CANTIDAD_DECIMALES);
+		Double re = formatearDecimales(0.0635*eqebeExp*eqebgExp, CANTIDAD_DECIMALES);
+		
+		Double npg = formatearDecimales(swg*(268-(29.4*(re/swg))), CANTIDAD_DECIMALES);
+		Double effmpNpg = formatearDecimales((83.4-(0.114*eqsbw))/100, CANTIDAD_DECIMALES);
+		
+		Double mpGrowth = 0.0;
+		if (entradaDto.getNumeroParto()<=2) {
+			mpGrowth = formatearDecimales(npg / effmpNpg, CANTIDAD_DECIMALES);
+		} else {
+			mpGrowth = 0.0;
+		}
+		
+		Double totalMpRequeriment = formatearDecimales(mpMaint + mpLact + mpPreg + mpGrowth, CANTIDAD_DECIMALES);
+		
+		modeloDto.setActualDMI(cmsActual);
 		modeloDto.setTotalDMFeed(totalDmFeed);
 		modeloDto.setScurfRequirement(scurfRequirement);
 		modeloDto.setUrinaryRequirement(urinaryRequirement);
+		modeloDto.setMetabolicFecalProteinReq(metabolicFecalProteinReq);
+		modeloDto.setMpEndougenosRequirement(mpEndoungeosRequirement);
+		modeloDto.setMpMaint(mpMaint);
+		modeloDto.setMpLact(mpLact);
+		modeloDto.setMpPreg(mpPreg);
+		modeloDto.setMpGrowth(mpGrowth);
+		modeloDto.setTotalMpRequirement(totalMpRequeriment);
 		return modeloDto;
 	}
 
