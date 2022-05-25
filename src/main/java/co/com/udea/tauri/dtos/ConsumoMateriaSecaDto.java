b0b96bcc958a64ad873e3735568d5b687e851f1a
Daniel Serna <daniel.serna@sancorsalud.com.ar>
2022-05-25 16:06:04 -0500

    consumo materia seca predicha

---
+++ src/main/java/co/com/udea/tauri/dtos/ConsumoMateriaSecaDto.java
@@ -0,0 +1,35 @@
+package co.com.udea.tauri.dtos;
+
+public class ConsumoMateriaSecaDto {
+	
+	private Double nrc;
+	
+	private Double nrcEfectosAnimales;
+	
+	private Double tuari;
+
+	public Double getNrc() {
+		return nrc;
+	}
+
+	public void setNrc(Double nrc) {
+		this.nrc = nrc;
+	}
+
+	public Double getNrcEfectosAnimales() {
+		return nrcEfectosAnimales;
+	}
+
+	public void setNrcEfectosAnimales(Double nrcEfectosAnimales) {
+		this.nrcEfectosAnimales = nrcEfectosAnimales;
+	}
+
+	public Double getTuari() {
+		return tuari;
+	}
+
+	public void setTuari(Double tuari) {
+		this.tuari = tuari;
+	}
+	
+}
